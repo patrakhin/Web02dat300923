970c0d8c256cc1ed4f9990bf1490b41370b783f3
patrakhin <patrakhin@mail.ru>
2023-09-30 22:12:15 +0300

    added 1st fix 221230092023

--- src/main/java/com/example/web02dat300923/Web02dat300923Application.java
+++ src/main/java/com/example/web02dat300923/Web02dat300923Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Web02dat300923Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Web02dat300923Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Web02dat300923Application.class, args);
+    }
 
 }
